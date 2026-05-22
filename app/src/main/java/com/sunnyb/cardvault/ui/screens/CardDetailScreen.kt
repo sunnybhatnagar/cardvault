058b5456546af727eb121d1a364c0931a2020587
Sunny Bhatnagar <bhatnagar.sunnyb10@gmail.com>
2026-05-23 01:16:03 +0530

    Remove unused LocalView import from CardDetailScreen

--- app/src/main/java/com/sunnyb/cardvault/ui/screens/CardDetailScreen.kt
+++ app/src/main/java/com/sunnyb/cardvault/ui/screens/CardDetailScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
